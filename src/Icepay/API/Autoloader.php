7d5c5198cc149a205eb3da61f3c03986f95961e5
Ricardo Jacobs <ricardozegt@gmail.com>
2015-09-29 10:51:26 +0200

    Initial work on the Autoloader (if not using Composer).

---
+++ src/Icepay/API/Autoloader.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * ICEPAY REST API for PHP
+ *
+ * @version     0.0.1
+ * @authors     Ricardo Jacobs <ricardozegt@gmail.com>
+ * @license     BSD-2-Clause, see LICENSE.md
+ * @copyright   (c) 2015, ICEPAY B.V. All rights reserved.
+ */
+
+class Autoloader
+{
+
+}
