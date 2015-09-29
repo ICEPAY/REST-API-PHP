4414fe4c41b7907b8bfbf62928957a8ecbe8ab7d
Ricardo Jacobs <ricardozegt@gmail.com>
2015-09-29 11:45:45 +0200

    Updating API resources.

---
+++ src/Icepay/API/Resources/Payment.php
@@ -0,0 +1,206 @@
+<?php namespace Icepay\API\Resources;
+
+/**
+ * ICEPAY REST API for PHP
+ *
+ * @version     0.0.1
+ * @authors     Ricardo Jacobs <ricardozegt@gmail.com>
+ * @license     BSD-2-Clause, see LICENSE.md
+ * @copyright   (c) 2015, ICEPAY B.V. All rights reserved.
+ */
+
+class Payment extends BaseApi
+{
+    /**
+     * @param $data
+     * @return mixed
+     */
+    public function checkOut($data)
+    {
+        /**
+         * Information for starting a payment
+         */
+        $information = array(
+            'Timestamp' => $this->getTimeStamp(),
+            'Amount' => $data['Amount'],
+            'Country' => $data['Country'],
+            'Currency' => $data['Currency'],
+            'Description' => $data['Description'],
+            'Issuer' => $data['Issuer'],
+            'Language' => $data['Language'],
+            'OrderID' => $data['OrderID'],
+            'PaymentMethod' => $data['Paymentmethod'],
+            'Reference' => $data['Reference'],
+            'URLCompleted' => $this->client->api_completed_url,
+            'URLError' => $this->client->api_error_url,
+            'EndUserIP' => $this->getClientIp()
+        );
+
+        /**
+         * Generate the checksum for the request
+         */
+        $checksum = $this->generateChecksum(
+            $this->client->api_endpoint .
+            'payment/checkout' .
+            $this->client->api_post .
+            $this->client->api_key .
+            $this->client->api_secret .
+            json_encode($information)
+        );
+
+        /**
+         * Make the call.
+         */
+        return $this->client->request($this->client->api_post, 'payment/checkout', $information, $checksum);
+    }
+
+    /**
+     * @param $data
+     * @return mixed
+     */
+    public function vaultCheckout($data)
+    {
+        /**
+         * Information for starting a recurring payment
+         */
+        $information = array(
+            'Timestamp' => $this->getTimeStamp(),
+            'Amount' => $data['Amount'],
+            'Country' => $data['Country'],
+            'Currency' => $data['Currency'],
+            'Description' => $data['Description'],
+            'Issuer' => $data['Issuer'],
+            'CustomerID' => $data['CustomerID'],
+            'Language' => $data['Language'],
+            'OrderID' => $data['OrderID'],
+            'PaymentMethod' => $data['Paymentmethod'],
+            'Reference' => $data['Reference'],
+            'URLCompleted' => $this->client->api_completed_url,
+            'URLError' => $this->client->api_error_url,
+            'EndUserIP' => $this->getClientIp()
+        );
+
+        /**
+         * Generate the checksum for the request
+         */
+        $checksum = $this->generateChecksum(
+            $this->client->api_endpoint .
+            'payment/vaultcheckout' .
+            $this->client->api_post .
+            $this->client->api_key .
+            $this->client->api_secret .
+            json_encode($information)
+        );
+
+        /**
+         * Make the call.
+         */
+        return $this->client->request($this->client->api_post, 'payment/vaultcheckout', $information, $checksum);
+    }
+
+    /**
+     * @param $data
+     * @return mixed
+     */
+    public function autoCheckout($data)
+    {
+        /**
+         * Information for continuing a recurring payment
+         */
+        $information = array(
+            'Timestamp' => $this->getTimeStamp(),
+            'Amount' => $data['Amount'],
+            'Country' => $data['Country'],
+            'Currency' => $data['Currency'],
+            'Description' => $data['Description'],
+            'Issuer' => $data['Issuer'],
+            'CustomerID' => $data['CustomerID'],
+            'Language' => $data['Language'],
+            'OrderID' => $data['OrderID'],
+            'PaymentMethod' => $data['Paymentmethod'],
+            'Reference' => $data['Reference'],
+            'URLCompleted' => $this->client->api_completed_url,
+            'URLError' => $this->client->api_error_url,
+            'EndUserIP' => $this->getClientIp()
+        );
+
+        /**
+         * Generate the checksum for the request
+         */
+        $checksum = $this->generateChecksum(
+            $this->client->api_endpoint .
+            'payment/automaticcheckout' .
+            $this->client->api_post .
+            $this->client->api_key .
+            $this->client->api_secret .
+            json_encode($information)
+        );
+
+        /**
+         * Make the call.
+         */
+        return $this->client->request($this->client->api_post, 'payment/automaticcheckout', $information, $checksum);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMyPaymentMethods()
+    {
+        /**
+         * Information for getting active payment methods
+         */
+        $information = array(
+            'Timestamp' => $this->getTimeStamp()
+        );
+
+        /**
+         * Generate the checksum for the request
+         */
+        $checksum = $this->generateChecksum(
+            $this->client->api_endpoint .
+            'payment/getmypaymentmethods' .
+            $this->client->api_post .
+            $this->client->api_key .
+            $this->client->api_secret .
+            json_encode($information)
+        );
+
+        /**
+         * Make the call.
+         */
+        return $this->client->request($this->client->api_post, 'payment/getmypaymentmethods', $information, $checksum);
+    }
+
+    /**
+     * @param $data
+     * @return mixed
+     */
+    public function getPayment($data)
+    {
+        /**
+         * Information for getting active payment methods
+         */
+        $information = array(
+            'Timestamp' => $this->getTimeStamp(),
+            'PaymentID' => $data['PaymentID']
+        );
+
+        /**
+         * Generate the checksum for the request
+         */
+        $checksum = $this->generateChecksum(
+            $this->client->api_endpoint .
+            'payment/getpayment' .
+            $this->client->api_post .
+            $this->client->api_key .
+            $this->client->api_secret .
+            json_encode($information)
+        );
+
+        /**
+         * Make the call.
+         */
+        return $this->client->request($this->client->api_post, 'payment/getpayment', $information, $checksum);
+    }
+}
---
+++ src/Icepay/API/Resources/Refund.php
@@ -0,0 +1,112 @@
+<?php namespace Icepay\API\Resources;
+
+/**
+ * ICEPAY REST API for PHP
+ *
+ * @version     0.0.1
+ * @authors     Ricardo Jacobs <ricardozegt@gmail.com>
+ * @license     BSD-2-Clause, see LICENSE.md
+ * @copyright   (c) 2015, ICEPAY B.V. All rights reserved.
+ */
+
+class Refund extends BaseApi
+{
+    /**
+     * @param $data
+     * @return mixed
+     */
+    public function startRefund($data)
+    {
+        /**
+         * Information for starting the refund
+         */
+        $information = array(
+            'Timestamp' => $this->getTimeStamp(),
+            'PaymentID' => $data['PaymentID'],
+            'RefundAmount' => $data['Amount'],
+            'RefundCurrency' => $data['Currency']
+        );
+
+        /**
+         * Generate the checksum for the request
+         */
+        $checksum = $this->generateChecksum(
+            $this->client->api_endpoint .
+            'refund/requestrefund' .
+            $this->client->api_post .
+            $this->client->api_key .
+            $this->client->api_secret .
+            json_encode($information)
+        );
+
+        /**
+         * Make the call.
+         */
+        return $this->client->request($this->client->api_post, 'refund/requestrefund', $information, $checksum);
+    }
+
+    /**
+     * @param $data
+     * @return mixed
+     */
+    public function cancelRefund($data)
+    {
+        /**
+         * Information for cancelling the refund
+         */
+        $information = array(
+            'Timestamp' => $this->getTimeStamp(),
+            'RefundID' => $data['RefundID'],
+            'PaymentID' => $data['PaymentID']
+        );
+
+        /**
+         * Generate the checksum for the request
+         */
+        $checksum = $this->generateChecksum(
+            $this->client->api_endpoint .
+            'refund/cancelrefund' .
+            $this->client->api_post .
+            $this->client->api_key .
+            $this->client->api_secret .
+            json_encode($information)
+        );
+
+        /**
+         * Make the call.
+         */
+        return $this->client->request($this->client->api_post, 'refund/cancelrefund', $information, $checksum);
+    }
+
+    /**
+     * @param $data
+     * @return mixed
+     */
+    public function getRefundStatus($data)
+    {
+        /**
+         * Information for cancelling the refund
+         */
+        $information = array(
+            'Timestamp' => $this->getTimeStamp(),
+            'PaymentID' => $data['PaymentID']
+        );
+
+        /**
+         * Generate the checksum for the request
+         */
+        $checksum = $this->generateChecksum(
+            $this->client->api_endpoint .
+            'refund/getpaymentrefunds' .
+            $this->client->api_post .
+            $this->client->api_key .
+            $this->client->api_secret .
+            json_encode($information)
+        );
+
+        /**
+         * Make the call.
+         */
+        return $this->client->request($this->client->api_post, 'refund/getpaymentrefunds', $information, $checksum);
+    }
+}
